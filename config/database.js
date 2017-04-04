7a960d2b3ca842dfca004e5b77c0a21d9c7f2338
George Paton <georgewpaton@me.com>
2017-04-04 22:12:24 +1000

    Update DB details

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://movieaccess:ameoEC2dsL@ec2-52-65-5-105.ap-southeast-2.compute.amazonaws.com:27017/movie',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
