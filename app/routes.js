6369014c32db40a31495768182726022f6f9cc4b
George Paton <georgewpaton@me.com>
2017-04-07 17:11:52 +1000

    Fix routing for pages
    The original developer made a small mistake, the ‘*’ was catching all
    requests, but since the index.html was at the root, it didn’t
    successfully fire the event. If this event fired successfully, ALL
    requests would return the index.html (including css, js, and API
    requests!)

--- app/routes.js
+++ app/routes.js
@@ -274,7 +274,11 @@ module.exports = function (app) {
     });
 
     // application -------------------------------------------------------------
-    app.get('*', function (req, res) {
-        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+	app.get('/movie/:movie_id', function (req, res) {
+		res.sendFile(__dirname + '/html/detail.html');
+	});
+	
+    app.get('/', function (req, res) {
+        res.sendFile(__dirname + '/html/index.html');
     });
 };
